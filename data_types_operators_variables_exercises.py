4b421221761e1829b9907d646ad1b888f995a3b0
David Ryan Alviola <david.r.alviola@accenturefederal.com>
2021-01-11 15:49:11 -0600

    Started working on data types, oeprators, variables exercises.

---
+++ data_types_operators_variables_exercises.py
@@ -0,0 +1,15 @@
+# data types, operators, and variables
+
+# Identify the data type of the following values:
+
+type(99.9) #float
+type("False") #str
+type(False) #bool
+type('0') #str
+type(0) #int
+type(True) #bool
+type('True') #str
+type([{}]) #list
+type({'a':[]}) #dict
+
+#
